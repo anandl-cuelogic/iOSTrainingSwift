6a66e275e937901c2c437b6da2636e01256633cc
Anand Lilhare <anand.lilhare@cuelogic.com>
2021-02-07 11:31:16 +0530

    Section 2 and Section 3 code

---
+++ I AM POOR/I AM POOR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM POOR
+//
+//  Created by Anand Lilhare on 07/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I AM RICH/I AM RICH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Anand Lilhare on 02/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
